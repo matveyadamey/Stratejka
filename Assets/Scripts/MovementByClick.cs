c239d6ccafe27be5cd16c69bc5fe92b4efef8af0
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2024-03-29 14:22:33 +0300

    movement

---
+++ Assets/Scripts/MovementByClick.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class MovementByClick : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Transform player;
+    private float aids=5;
+
+    private IEnumerator MoveObject(Vector3 target)
+    {
+        while (Vector3.Distance(player.position, target) > 0.1f)
+        {
+            float speed = aids * Time.deltaTime;
+            player.localPosition = Vector3.Lerp(player.localPosition, target, speed);
+
+            yield return null;
+        }
+    }
+
+
+
+
+    public void OnPointerClick(PointerEventData eventdata)
+    {
+        StartCoroutine(MoveObject(new Vector3(transform.position.x,1, transform.position.z)));
+    } 
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
