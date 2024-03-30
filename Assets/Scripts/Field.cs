870db8fde28f64d4557be589f1c5d375e845368d
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2024-03-30 10:17:15 +0300

    Spawn field
    ееееееее

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class Field : MonoBehaviour
 {
     const int SIZE=10;
-    GameObject[][] coordNet=new GameObject[SIZE][];
-    [SerializeField] private GameObject cubes;
-    private void Start()
+    GameObject[][] coordNet = new GameObject[SIZE][];
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private Material[] materials;
+    [SerializeField] private Transform field;
+    public GameObject getObject(int x, int y)
+    {
+        return coordNet[x][y];
+    }
+    Material GetMaterial(int x,int y)
+    {
+        int matInd=Mathf.Min(Mathf.Min(x, SIZE-x-1),Mathf.Min(y,SIZE-y-1));
+        return materials[matInd];
+
+    }
+    public void setColor(int x, int y, Material mat)
+    {
+        getObject(x, y).GetComponent<Renderer>().material = mat;
+    }
+    void Start()
     {
         for (int i = 0; i < SIZE; i++)
         {
             coordNet[i] = new GameObject[SIZE];
             for (int j = 0; j < SIZE; j++)
             {
-                coordNet[i][j] = cubes.transform.GetChild(i).gameObject.transform.GetChild(j).gameObject;
+                GameObject cube = Instantiate(cubePrefab, new Vector3(i, 0, j), Quaternion.identity,field);
+                coordNet[i][j] = cube;
+                Material mat = GetMaterial(i, j);
+                setColor(i, j, mat);
             }
         }
-    }
-    public GameObject getObjectByCoord(int x,int y)
-    {
-        return coordNet[x][y];
+
     }
 
 }
