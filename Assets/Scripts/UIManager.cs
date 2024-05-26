ed214be6f8ff73b06e11989b9b0e0fe1b7229d24
RazWars <156277907+RazWars@users.noreply.github.com>
2024-05-26 15:06:52 +0300

    Счётчик монет

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -6,6 +8,16 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject _turretPrefab;
     [SerializeField] private int blockCost;
     [SerializeField] private int turretCost;
+    [SerializeField] private Text Money1;
+    [SerializeField] private Text Money2;
+
+    private void Update()
+    {
+        Player player1 = PlayersContainer.Players[0];
+        Player player2 = PlayersContainer.Players[1];
+        Money1.text = player1.CountCoins.ToString();
+        Money2.text = player2.CountCoins.ToString();
+    }
 
     public void BuyButton(int cost, GameObject prefab)
     {
